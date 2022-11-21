469fea983c032c29541185a63b1e70ac27c1e836
Hauke Bartsch <HaukeBartsch@gmail.com>
2022-11-21 23:35:57 +0100

    add quartiles q1 and q3

--- pr2mask.cxx
+++ pr2mask.cxx
@@ -915,6 +915,45 @@ ImageType2D::Pointer createMaskFromStorage(ImageType2D::Pointer im2change, std::
   return mask;
 }
 
+template<typename T>
+static inline double Lerp(T v0, T v1, T t) {
+    return (1 - t)*v0 + t*v1;
+}
+
+template<typename T, typename U>
+static inline std::vector<T> Quantile(const std::vector<U>& inData, const std::vector<T>& probs) {
+    if (inData.empty())
+    {
+        return std::vector<T>();
+    }
+
+    if (1 == inData.size())
+    {
+        return std::vector<T>(1, inData[0]);
+    }
+
+    std::vector<U> data = inData;
+    std::sort(data.begin(), data.end());
+    std::vector<T> quantiles;
+
+    for (size_t i = 0; i < probs.size(); ++i)
+    {
+        T poi = Lerp<T>(-0.5, data.size() - 0.5, probs[i]);
+
+        size_t left = std::max(int64_t(std::floor(poi)), int64_t(0));
+        size_t right = std::min(int64_t(std::ceil(poi)), int64_t(data.size() - 1));
+
+        T datLeft = data.at(left);
+        T datRight = data.at(right);
+
+        T quantile = Lerp<T>(datLeft, datRight, poi - left);
+
+        quantiles.push_back(quantile);
+    }
+
+    return quantiles;
+}
+
 void computeBiomarkers(Report *report, std::string output_path, std::string imageSeries, std::string labelSeries) {
   typedef itk::GDCMSeriesFileNames NamesGeneratorType;
   NamesGeneratorType::Pointer nameGenerator = NamesGeneratorType::New();
@@ -1231,6 +1270,24 @@ std::cout<<featureCalc->GetClusterShade();
       }
       sum2 /= (pixelValues.size()-1);
       stdev = std::sqrt(sum2);
+
+      // compute the Q1 and Q3 as well
+      auto quartiles = Quantile<double, int>(pixelValues, { 0.25, 0.5, 0.75 });
+
+      buf.str("");
+      buf << quartiles[0];
+      meas->insert(std::make_pair("image_intensity_q1" , buf.str()));
+      buf.str("");
+      buf << "    Q1 intensity: " << quartiles[0];
+      report->summary.push_back(buf.str());
+
+      buf.str("");
+      buf << quartiles[2];
+      meas->insert(std::make_pair("image_intensity_q3" , buf.str()));
+      buf.str("");
+      buf << "    Q3 intensity: " << quartiles[2];
+      report->summary.push_back(buf.str());
+
 
       buf.str("");
       buf << stdev;
